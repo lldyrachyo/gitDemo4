ac0738277099352b72ca86aab9d0b9bc8c4e2f58
Mark <papushman@yandex.ru>
2023-08-18 16:49:49 +0300

    Initial Commit

---
+++ gitDemo4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  gitDemo4
+//
+//  Created by Mark Papush on 8/18/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
